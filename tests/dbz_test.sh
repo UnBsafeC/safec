8f717b7d87c5f37ae1fa7e32437e4395ee723b0e
David Carlos <ddavidcarlos1392@gmail.com>
2015-05-10 11:35:20 -0300

    Removes unnecessary examples directory
    Signed-off-by: David Carlos <ddavidcarlos1392@gmail.com>

--- tests/dbz_test.sh
+++ tests/dbz_test.sh
@@ -1,19 +1,20 @@
 . $(dirname $0)/helper/test_helper.sh
 
+. $(dirname $0)/helper/dbz_variables.sh
+
 test_explicit_and_simple_dbz() {
-  out="$(echo {1/0} | ./safec)"
+  out="$(echo $simple_dbz | ./safec)"
   assertEquals "$out" "Divisão por zero encontrada!!"
 }
 
 test_simple_expression_with_dbz() {
-  out="$(echo {2/\(5-5\)} | ./safec)"
+  out="$(echo $simple_expression_with_dbz | ./safec)"
   assertEquals "$out" "Divisão por zero encontrada!!"
 }
 
 test_more_complex_expression_with_dbz() {
-  out="$(./safec tests/examples/dbz)"
-  out_without_spaces="$(echo $out | sed -e 's/^[ \t]*//')"
-  assertEquals "$out_without_spaces" "Divisão por zero encontrada!!"
+  out="$(echo $more_complex_expression_with_dbz | ./safec)"
+  assertEquals "$out" "Divisão por zero encontrada!!"
 }
 
 load_shunit2
