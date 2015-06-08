0a0892ef98c4bc80d088a5319592cdb92272c16f
David Carlos <ddavidcarlos1392@gmail.com>
2015-06-08 00:25:57 -0300

    removes output dir with make clean
    Signed-off-by: David Carlos <ddavidcarlos1392@gmail.com>

--- tests/helper/dbz.c
+++ tests/helper/dbz.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 int sum(x,y,z){
-
     x/y;
     z;
     int a = 2/0;
@@ -11,13 +10,15 @@ int sum(x,y,z){
 int main(){
     int x = 2;
     int y = 0;
+
     int z;
     int g = 2/5;
-    sum(x,y,z);
 
+    sum(x,y,z);
     int g = 10;
+
     int p = 0;
     int g = g/(p);
 
+    int f = 4/((pow(2,2)) - 4);
 }
-
