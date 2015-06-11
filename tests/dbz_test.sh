cb96bb28df04c2b96c5297a29cbff43d804c49b6
Thiago Ribeiro <thiagitosouza@hotmail.com>
2015-06-11 17:53:07 -0300

    Adding functional tests to divizion by zero.
    Signed-off-by: Thiago Ribeiro <thiagitosouza@hotmail.com>

--- tests/dbz_test.sh
+++ tests/dbz_test.sh
@@ -2,30 +2,46 @@
 
 . $(dirname $0)/helper/dbz_variables.sh
 
+
 test_explicit_and_simple_dbz() {
-  out="$(echo $simple_dbz | ./safec)"
-  assertEquals "$out" "Divisão por zero encontrada!!"
+  ./safec tests/helper/dbz_test_case/simple_dbz.c
+  output=$(cat output/safec.c )
+  msg="/*Divisao explicita por zero encontrada!!*/"
+  output=*$msg*
+  assertEquals "$output" "*$msg*"
 }
 
 test_simple_expression_with_dbz() {
-  out="$(echo $simple_expression_with_dbz | ./safec)"
-  assertEquals "$out" "Divisão por zero encontrada!!"
+  ./safec tests/helper/dbz_test_case/simple_expression_with_dbz.c
+  output=$(cat output/safec.c )
+  msg="/*Divisao explicita por zero encontrada!!*/"
+  output=*$msg*
+  assertEquals "$output" "*$msg*"
 }
 
 test_more_complex_expression_with_dbz() {
-  out="$(echo $more_complex_expression_with_dbz | ./safec)"
-  assertEquals "$out" "Divisão por zero encontrada!!"
+  ./safec tests/helper/dbz_test_case/more_complex_experssion_with_dbz.c
+  output=$(cat output/safec.c )
+  msg="/*Divisao por zero encontrada!!*/"
+  output=*$msg*
+  assertEquals "$output" "*$msg*"
 }
 
 test_declare_variable_with_zero() {
-  out="$(echo $declare_variable_with_zero | ./safec)"
-  assertEquals "$out" "Divisão por zero encontrada!!"
+  ./safec tests/helper/dbz_test_case/declare_variable_with_zero.c
+  output=$(cat output/safec.c )
+  msg="/*Divisao por zero encontrada!!*/"
+  output=*$msg*
+  assertEquals "$output" "*$msg*"
 }
 
 
 test_declare_division_by_zero_as_expression() {
-  out="$(echo $declare_division_by_zero_as_expression | ./safec)"
-  assertEquals "$out" "Divisão por zero encontrada!!"
+  ./safec tests/helper/dbz_test_case/declare_division_by_zero_as_expression.c
+  output=$(cat output/safec.c )
+  msg="/*Divisao por zero encontrada!!*/"
+  output=*$msg*
+  assertEquals "$output" "*$msg*"
 }
 
 load_shunit2
