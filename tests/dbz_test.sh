691decc4223ce07586c2dac0b4acab0b8649a5dc
Carolina Ramalho <carol15022@hotmail.com>
2015-06-15 09:11:53 -0300

    Improvement tests and fix segmentation fault in dbz.
    Signed-off-by: Thiago Ribeiro <thiagitosouza@hotmail.com>

--- tests/dbz_test.sh
+++ tests/dbz_test.sh
@@ -5,43 +5,38 @@
 
 test_explicit_and_simple_dbz() {
   ./safec tests/helper/dbz_test_case/simple_dbz.c
-  output=$(cat output/safec.c )
+  output=$(grep -r "Divisao explicita por zero encontrada" output/safec.c)
   msg="/*Divisao explicita por zero encontrada!!*/"
-  output=*$msg*
-  assertEquals "$output" "*$msg*"
+  assertEquals "$output" "$msg"
 }
 
 test_simple_expression_with_dbz() {
   ./safec tests/helper/dbz_test_case/simple_expression_with_dbz.c
-  output=$(cat output/safec.c )
+  output=$(grep -r "Divisao explicita por zero encontrada" output/safec.c )
   msg="/*Divisao explicita por zero encontrada!!*/"
-  output=*$msg*
-  assertEquals "$output" "*$msg*"
+  assertEquals "$output" "$msg"
 }
 
 test_more_complex_expression_with_dbz() {
-  ./safec tests/helper/dbz_test_case/more_complex_experssion_with_dbz.c
-  output=$(cat output/safec.c )
-  msg="/*Divisao por zero encontrada!!*/"
-  output=*$msg*
-  assertEquals "$output" "*$msg*"
+  ./safec tests/helper/dbz_test_case/more_complex_expression_with_dbz.c
+  output=$(grep -r "Divisao explicita por zero encontrada" output/safec.c )
+  msg="/*Divisao explicita por zero encontrada!!*/"
+  assertEquals "$output" "$msg"
 }
 
 test_declare_variable_with_zero() {
   ./safec tests/helper/dbz_test_case/declare_variable_with_zero.c
-  output=$(cat output/safec.c )
-  msg="/*Divisao por zero encontrada!!*/"
-  output=*$msg*
-  assertEquals "$output" "*$msg*"
+  output=$(grep -r "Divisao invalida: Voce inicializou a variavel a com zero" output/safec.c )
+  msg="/*Divisao invalida: Voce inicializou a variavel a com zero!!*/"
+  assertEquals "$output" "$msg"
 }
 
 
 test_declare_division_by_zero_as_expression() {
   ./safec tests/helper/dbz_test_case/declare_division_by_zero_as_expression.c
-  output=$(cat output/safec.c )
-  msg="/*Divisao por zero encontrada!!*/"
-  output=*$msg*
-  assertEquals "$output" "*$msg*"
+  output=$(grep -r "Divisao invalida: Voce inicializou a variavel n com zero" output/safec.c )
+  msg="/*Divisao invalida: Voce inicializou a variavel n com zero!!*/"
+  assertEquals "$output" "$msg"
 }
 
 load_shunit2
